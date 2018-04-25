590b73e1b39852fa5c01d2afcd16ba8506a103c2
anjohan <anjohan@uio.no>
2018-04-25 19:56:23 +0200

    c) ca. ferdig, flyttet lapackrot til modul

--- utilities.f90
+++ utilities.f90
@@ -22,4 +22,37 @@ module utilities
             dx = (b - a)/(N - 1)
             linspace = [ (a + i*dx, i=0, N-1) ]
         end function
+        subroutine linfit(x, y, slope, const)
+            real(kind=dp), dimension(:), intent(in) :: x, y
+            real(kind=dp), intent(out) :: slope, const
+            real(kind=dp), dimension(:,:), allocatable :: A
+            real(kind=dp), dimension(:), allocatable :: b, work
+            integer :: lda, ldb, lwork, info, m
+
+            m = size(x)
+            allocate(A(m,2))
+            allocate(b(m))
+            allocate(work(1))
+
+            A(:,1) = x(:)
+            A(:,2) = 1
+            b(:) = y(:)
+
+            lda = m
+            ldb = m
+
+            lwork = -1
+            call dgels("N",m,2,1,A,lda,b,ldb,work,lwork,info)
+            lwork = int(work(1))
+            deallocate(work)
+            allocate(work(lwork))
+            call dgels("N",m,2,1,A,lda,b,ldb,work,lwork,info)
+
+            slope = b(1)
+            const = b(2)
+
+            if(info /= 0) then
+                error stop "Problems with least squares"
+            endif
+        end subroutine
 end module
