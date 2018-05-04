60eef7bbd33e9f48535fdd33fae34abdedeca6a8
Anders Johansson <anjohan@uio.no>
2018-05-04 13:21:02 +0200

    proper indentation

--- hk.c
+++ hk.c
@@ -42,47 +42,47 @@ int  n_labels = 0;     /* length of the labels array */
 /*  uf_find returns the canonical label for the equivalence class containing x */
 
 int uf_find(int x) {
-  int y = x;
-  while (labels[y] != y)
-    y = labels[y];
+    int y = x;
+    while (labels[y] != y)
+        y = labels[y];
 
-  while (labels[x] != x) {
-    int z = labels[x];
-    labels[x] = y;
-    x = z;
-  }
-  return y;
+    while (labels[x] != x) {
+        int z = labels[x];
+        labels[x] = y;
+        x = z;
+    }
+    return y;
 }
 
 /*  uf_union joins two equivalence classes and returns the canonical label of the resulting class. */
 
 int uf_union(int x, int y) {
-  return labels[uf_find(x)] = uf_find(y);
+    return labels[uf_find(x)] = uf_find(y);
 }
 
 /*  uf_make_set creates a new equivalence class and returns its label */
 
 int uf_make_set(void) {
-  labels[0] ++;
-  assert(labels[0] < n_labels);
-  labels[labels[0]] = labels[0];
-  return labels[0];
+    labels[0] ++;
+    assert(labels[0] < n_labels);
+    labels[labels[0]] = labels[0];
+    return labels[0];
 }
 
 /*  uf_intitialize sets up the data structures needed by the union-find implementation. */
 
 void uf_initialize(int max_labels) {
-  n_labels = max_labels;
-  labels = calloc(sizeof(int), n_labels);
-  labels[0] = 0;
+    n_labels = max_labels;
+    labels = calloc(sizeof(int), n_labels);
+    labels[0] = 0;
 }
 
 /*  uf_done frees the memory used by the union-find data structures */
 
 void uf_done(void) {
-  n_labels = 0;
-  free(labels);
-  labels = 0;
+    n_labels = 0;
+    free(labels);
+    labels = 0;
 }
 
 /* End Union-Find implementation */
@@ -95,11 +95,11 @@ void uf_done(void) {
    arrays, but allows for 2D arrays with dimensions determined at run-time */
 
 void print_matrix(int *matrix, int m, int n) {
-  for (int i=0; i<m; i++) {
-    for (int j=0; j<n; j++)
-      printf("%3d ",matrix[(i)*m+j]);
-    printf("\n");
-  }
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++)
+            printf("%3d ",matrix[(i)*m+j]);
+        printf("\n");
+    }
 }
 
 
@@ -107,79 +107,79 @@ void print_matrix(int *matrix, int m, int n) {
 
 int hoshen_kopelman(int *matrix, int m, int n) {
 
-  uf_initialize(m * n / 2);
+    uf_initialize(m * n / 2);
 
-  /* scan the matrix */
+    /* scan the matrix */
 
-  for (int i=0; i<m; i++)
-    for (int j=0; j<n; j++)
-      if (matrix[(i)*m+j]) {                        // if occupied ...
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            if (matrix[(i)*m+j]) {                        // if occupied ...
 
-	int up = (i==0 ? 0 : matrix[(i-1)*m+j]);    //  look up
-	int left = (j==0 ? 0 : matrix[(i)*m+j-1]);  //  look left
+                int up = (i==0 ? 0 : matrix[(i-1)*m+j]);    //  look up
+                int left = (j==0 ? 0 : matrix[(i)*m+j-1]);  //  look left
 
-	switch (!!up + !!left) {
+                switch (!!up + !!left) {
 
-	case 0:
-	  matrix[(i)*m+j] = uf_make_set();      // a new cluster
-	  break;
+                    case 0:
+                        matrix[(i)*m+j] = uf_make_set();      // a new cluster
+                        break;
 
-	case 1:                              // part of an existing cluster
-	  matrix[(i)*m+j] = max(up,left);       // whichever is nonzero is labelled
-	  break;
+                    case 1:                              // part of an existing cluster
+                        matrix[(i)*m+j] = max(up,left);       // whichever is nonzero is labelled
+                        break;
 
-	case 2:                              // this site binds two clusters
-	  matrix[(i)*m+j] = uf_union(up, left);
-	  break;
-	}
+                    case 2:                              // this site binds two clusters
+                        matrix[(i)*m+j] = uf_union(up, left);
+                        break;
+                }
 
-      }
+            }
 
-  /* apply the relabeling to the matrix */
+    /* apply the relabeling to the matrix */
 
-  /* This is a little bit sneaky.. we create a mapping from the canonical labels
-     determined by union/find into a new set of canonical labels, which are
-     guaranteed to be sequential. */
+    /* This is a little bit sneaky.. we create a mapping from the canonical labels
+       determined by union/find into a new set of canonical labels, which are
+       guaranteed to be sequential. */
 
-  int *new_labels = calloc(sizeof(int), n_labels); // allocate array, initialized to zero
+    int *new_labels = calloc(sizeof(int), n_labels); // allocate array, initialized to zero
 
-  for (int i=0; i<m; i++)
-    for (int j=0; j<n; j++)
-      if (matrix[(i)*m+j]) {
-	int x = uf_find(matrix[(i)*m+j]);
-	if (new_labels[x] == 0) {
-	  new_labels[0]++;
-	  new_labels[x] = new_labels[0];
-	}
-	matrix[(i)*m+j] = new_labels[x];
-      }
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            if (matrix[(i)*m+j]) {
+                int x = uf_find(matrix[(i)*m+j]);
+                if (new_labels[x] == 0) {
+                    new_labels[0]++;
+                    new_labels[x] = new_labels[0];
+                }
+                matrix[(i)*m+j] = new_labels[x];
+            }
 
-  int total_clusters = new_labels[0];
+    int total_clusters = new_labels[0];
 
-  free(new_labels);
-  uf_done();
+    free(new_labels);
+    uf_done();
 
-  return total_clusters;
+    return total_clusters;
 }
 
 /* This procedure checks to see that any occupied neighbors of an occupied site
    have the same label. */
 
 void check_labelling(int **matrix, int m, int n) {
-  int N,S,E,W;
-  for (int i=0; i<m; i++)
-    for (int j=0; j<n; j++)
-      if (matrix[(i)*m+j]) {
-	N = ( i==0 ? 0 : matrix[(i-1)*m+j] );
-	S = ( i==m-1 ? 0 : matrix[(i+1)*m+j] );
-	E = ( j==n-1 ? 0 : matrix[(i)*m+j+1] );
-	W = ( j==0 ? 0 : matrix[(i)*m+j-1] );
+    int N,S,E,W;
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            if (matrix[(i)*m+j]) {
+                N = ( i==0 ? 0 : matrix[(i-1)*m+j] );
+                S = ( i==m-1 ? 0 : matrix[(i+1)*m+j] );
+                E = ( j==n-1 ? 0 : matrix[(i)*m+j+1] );
+                W = ( j==0 ? 0 : matrix[(i)*m+j-1] );
 
-	assert( N==0 || matrix[(i)*m+j]==N );
-	assert( S==0 || matrix[(i)*m+j]==S );
-	assert( E==0 || matrix[(i)*m+j]==E );
-	assert( W==0 || matrix[(i)*m+j]==W );
-      }
+                assert( N==0 || matrix[(i)*m+j]==N );
+                assert( S==0 || matrix[(i)*m+j]==S );
+                assert( E==0 || matrix[(i)*m+j]==E );
+                assert( W==0 || matrix[(i)*m+j]==W );
+            }
 }
 
 /* The sample program reads in a matrix from standard input, runs the HK algorithm on
@@ -187,39 +187,39 @@ void check_labelling(int **matrix, int m, int n) {
    dimensions of the matrix, followed by the matrix elements (with data separated by
    whitespace).
 
-a sample input file is the following:
+   a sample input file is the following:
 
-8 8
-1 1 1 1 1 1 1 1
-0 0 0 0 0 0 0 1
-1 0 0 0 0 1 0 1
-1 0 0 1 0 1 0 1
-1 0 0 1 0 1 0 1
-1 0 0 1 1 1 0 1
-1 1 1 1 0 0 0 1
-0 0 0 1 1 1 0 1
+   8 8
+   1 1 1 1 1 1 1 1
+   0 0 0 0 0 0 0 1
+   1 0 0 0 0 1 0 1
+   1 0 0 1 0 1 0 1
+   1 0 0 1 0 1 0 1
+   1 0 0 1 1 1 0 1
+   1 1 1 1 0 0 0 1
+   0 0 0 1 1 1 0 1
 
-this sample input gives the following output:
+   this sample input gives the following output:
 
- --input--
-  1   1   1   1   1   1   1   1
-  0   0   0   0   0   0   0   1
-  1   0   0   0   0   1   0   1
-  1   0   0   1   0   1   0   1
-  1   0   0   1   0   1   0   1
-  1   0   0   1   1   1   0   1
-  1   1   1   1   0   0   0   1
-  0   0   0   1   1   1   0   1
- --output--
-  1   1   1   1   1   1   1   1
-  0   0   0   0   0   0   0   1
-  2   0   0   0   0   2   0   1
-  2   0   0   2   0   2   0   1
-  2   0   0   2   0   2   0   1
-  2   0   0   2   2   2   0   1
-  2   2   2   2   0   0   0   1
-  0   0   0   2   2   2   0   1
-HK reports 2 clusters found
+   --input--
+   1   1   1   1   1   1   1   1
+   0   0   0   0   0   0   0   1
+   1   0   0   0   0   1   0   1
+   1   0   0   1   0   1   0   1
+   1   0   0   1   0   1   0   1
+   1   0   0   1   1   1   0   1
+   1   1   1   1   0   0   0   1
+   0   0   0   1   1   1   0   1
+   --output--
+   1   1   1   1   1   1   1   1
+   0   0   0   0   0   0   0   1
+   2   0   0   0   0   2   0   1
+   2   0   0   2   0   2   0   1
+   2   0   0   2   0   2   0   1
+   2   0   0   2   2   2   0   1
+   2   2   2   2   0   0   0   1
+   0   0   0   2   2   2   0   1
+   HK reports 2 clusters found
 
 */
 
