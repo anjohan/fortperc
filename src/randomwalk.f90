1fda502888a3c6b4afd0107a843d3dd19bdbadba
Anders Johansson <anjohan@uio.no>
2018-06-11 19:54:50 +0200

    probdist seems to work with signed distance

--- src/randomwalk.f90
+++ src/randomwalk.f90
@@ -122,4 +122,103 @@ module randomwalk
             displacement(:) = displacement(:)/(num_walkers*num_systems)
 
         end function
+
+        function probability_distribution(p, L, num_steps, num_walkers, num_systems, num_hists, num_bins) &
+                 result(result_hist)
+            !! Start **num_walkers** on the percolating cluster of each of **num_systems**
+            !! systems, and compute **num_hists** histograms of the distribution
+            !! of particles.
+
+            real(kind=dp), intent(in) :: p
+                !! The probability for each site to allow transport.
+            integer, intent(in) ::  L
+                !! The system size.
+            integer, intent(in) ::  num_systems
+                !! The number of systems over which to average.
+            integer, intent(in) ::  num_walkers
+                !! The number of random walkers for each system over which to average.
+            integer, intent(in) ::  num_steps
+                !! The number of steps which the random walkers take.
+            integer, intent(in) :: num_hists
+                !! The number of histograms to be returned.
+            integer, intent(in) :: num_bins
+                !! The number of bins in the returned histograms.
+
+            real(kind=dp), dimension(:,:), allocatable :: result_hist
+                !! Array of histograms. The first row contains the centres of the
+                !! bins, while the remaining **num_hists+1** rows each contain
+                !! one histogram.
+
+
+            logical, dimension(:,:), allocatable :: binary_matrix, spanning_cluster
+            integer, dimension(:,:), allocatable :: label_matrix
+            integer, dimension(:), allocatable :: hist_indices
+            integer :: hist_dist, i, j, maxdist, x_index, y_index
+            real(kind=dp), dimension(:,:), allocatable :: tmp_displacement, tmp_hist
+            integer, dimension(:,:,:), allocatable :: displacement, absdisplacement
+
+
+
+            integer :: num_spanning_clusters, num_clusters, spanning_cluster_label
+
+            allocate(displacement(2,0:num_hists,num_walkers*num_systems))
+            hist_dist = num_steps/num_hists
+            allocate(hist_indices(0:num_hists))
+            hist_indices(:) = [(hist_dist*i, i=0, num_hists)]
+
+            num_spanning_clusters = 0
+            do while(num_spanning_clusters < num_systems)
+                binary_matrix = create_binary_matrix(p, L)
+                call label(binary_matrix, label_matrix, num_clusters)
+                spanning_cluster_label = find_spanning_cluster(label_matrix, num_clusters)
+
+                if(spanning_cluster_label < 1) then
+                    cycle
+                end if
+                num_spanning_clusters = num_spanning_clusters + 1
+
+                spanning_cluster = mark_percolating_with_periodic( &
+                                        label_matrix, spanning_cluster_label, &
+                                        num_clusters)
+
+
+                !$omp parallel do private(tmp_displacement)
+                do i = 1, num_walkers
+                    tmp_displacement = one_random_walker(spanning_cluster, num_steps)
+                    do j = 0, num_hists
+                        associate(hist_index => hist_indices(j), &
+                                  indx => (num_spanning_clusters-1)*num_walkers+i)
+                            displacement(:,j,indx) = tmp_displacement(:,hist_index+1)
+                        end associate
+                    end do
+                end do
+                !$omp end parallel do
+            end do
+
+            allocate(absdisplacement(2,0:num_hists,num_walkers*num_systems))
+
+            !$omp parallel workshare
+            absdisplacement(:,:,:) = abs(displacement(:,:,:))
+            maxdist = maxval(absdisplacement)
+            !$omp end parallel workshare
+
+            allocate(result_hist(-1:num_hists,-maxdist:maxdist))
+            result_hist(-1,:) = [(i, i = -maxdist, maxdist)]
+            result_hist(0:,:) = 0
+
+            !$omp parallel do private(x_index, y_index) reduction(+:result_hist)
+            do i = 0, num_hists
+                do j = 1, num_systems*num_walkers
+                    associate(x => displacement(1,i,j), &
+                              y => displacement(2,i,j))
+                        result_hist(i,x) = result_hist(i,x) + 1
+                        result_hist(i,y) = result_hist(i,y) + 1
+                    end associate
+                end do
+            end do
+            !$omp end parallel do
+
+            result_hist(0:,:) = result_hist(0:,:)/(2.0d0*num_systems*num_walkers)
+
+        end function
 end module randomwalk
